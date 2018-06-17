64ef26fe0ac0b0bb548681d8ba64e3ad4c0d27bf
Asteria <dcdion22@gmail.com>
2018-06-16 20:25:24 -0400

    Jun16 InProgress

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,52 +11,58 @@ $(document).ready(function () {
     var results = $("#results");
 
     var questionArr = [{
-            question: "Which year was Fallout 3 released?",
-            choice: ["2006", "2010", "2008", "2012"],
+            question: "In Fallout 3, what collection of books did Moria Brown write?",
+            choice: ["Tales of a Junktown Jerky Vendor", "Guns and Bullets", "Wasteland Survival Guide", "Grognak the Barbarian"],
             answer: 2,
-            photo: ""
+            photo: "assets/images/wlsg.png"
         },
         {
             question: "Which Assassins Creed game takes place in Ptolemaic, Egypt?",
             choice: ["Assassin's Creed: Revelations", "Assassin's Creed: Origins", "Assassin's Creed: Unity", "Assassin's Creed: Syndicate"],
             answer: 1,
-            photo: ""
+            photo: "assets/images/asas2.png"
         },
         {
             question: "What does Mario jump on after completing a level??",
             choice: ["A Big Mushroom", "A Coin Box", "A Piranha Plant", "A Flag Pole"],
             answer: 3,
-            photo: ""
+            photo: "assets/images/mfp.jpg"
         },
         {
-            question: "What does NES stand for?",
-            choice: ["Nintendo Entertainment System ", "Not Elsewhere Specified", "Never Ending Story", "Non Exciting Stuff"],
+            question: "What is the name of the missing girl in Life Is Strange?",
+            choice: ["Rachel Amber", "Victoria Chase", "Kate Marsh", "Juliet Watson"],
             answer: 0,
-            photo: ""
+            photo: "assets/images/ra.png"
         },
         {
-            question: "What year was the ps4 released?",
-            choice: ["2012", "2013", "2015", "2010"],
+            question: "What is the name of the giant robot-like creature in Bioshock 1 & 2?",
+            choice: ["Heavenly Father", "Big Daddy", "Bubble Jug", "Big Boi"],
             answer: 1,
-            photo: ""
+            photo: "assets/images/bd.jpg"
         },
         {
             question: "What color are Spyro The Dragons' wings?",
             choice: ["Blue", "Purple", "Orange", "Green"],
             answer: 2,
-            photo: ""
+            photo: "assets/images/sd2.jpg"
         },
         {
-            question: "What is Laura Craft's field of study in the Tomb Raider Series",
+            question: "What is Laura Croft's field of study in the Tomb Raider Series",
             choice: ["Agriculture", "Anthropology", "Architecture", "Archaeology"],
             answer: 3,
-            photo: ""
+            photo: "assets/images/lctr2.jpg"
         },
         {
-            question: "Who is the main protagonist	in God of War Series",
+            question: "Who is the main protagonist in God of War?",
             choice: ["Kratos", "Ares", "Zeus", "Athena"],
             answer: 0,
-            photo: ""
+            photo: "assets/images/kgw.jpg"
+        },
+        {
+            question: "From the Sonic series, what is Tails the Fox's real name?",
+            choice: ["Jonathan Prowler", "Tyler Posner", "Miles Prower", "Michael Ramsey"],
+            answer: 2,
+            photo: "assets/images/tails.png"
         }
     ];
 
@@ -92,31 +98,59 @@ $(document).ready(function () {
         $("#questionArea").html("<h2>" + questionM + "</h2>");
         for (var i = 0; i < answerM.choice.length; i++) {
 
-            var answerOption = $("<div>");//$("<button>")
+            var answerOption = $("<div>"); //$("<button>")
             answerOption.addClass("answerChoice btn"); //btn btn-primary
             answerOption.text(answerM.choice[i]);
             answerOption.attr("data-index", i);
             $("#answerArea").append(answerOption);
         }
         timer();
-        $(".answerChoice").on("click", function() {
+        $(".answerChoice").on("click", function () {
             var userSelect = $(this).data("index");
             console.log(userSelect);
+            console.log(answerM.answer)
+
+            if (userSelect == answerM.answer) {
+                counter++;
+                correctNum++;
+                stop()
+                var hurray = $("<h3>Correct!</h3>");
+                $("#answerArea").html(hurray);
+                var imageReplace = $("<img>");
+                imageReplace.attr("src", answerM.photo);
+                imageReplace.addClass("col-12 col-lg-6");
+                $("#answerArea").append(imageReplace);
+
+            } else if (userSelect != answerM.answer) {
+                counter++;
+                incorrectNum++;
+                stop()
+                var boo = $("<h3>Wrong! The correct answer was " + answerM.choice[answerM.answer] + "</h3>");
+                $("#answerArea").html(boo);
+                var imageReplace = $("<img>");
+                imageReplace.attr("src", answerM.photo);
+                imageReplace.addClass("col-12 col-lg-6");
+                $("#answerArea").append(imageReplace);
+            } //else if (countdown === 0) {
+            //     counter++;
+            //     unansweredNum++;
+            //     stop()
+            //     var tooLate = $("<h3>Times Up! The correct answer was " + answerM.choice[answerM.answer] + "</h3>");
+            //     $("#answerArea").html(tooLate);
+            //     var imageReplace = $("<img>");
+            //     imageReplace.attr("src", answerM.photo);
+            //     imageReplace.addClass("col-12 col-lg-6");
+            //     $("#answerArea").append(imageReplace);
+            // }
         })
     }
-
-
-    // $(".answerChoice").on("click", function() {
-    //     var userSelect = $(this).data("index");
-    //     console.log(userSelect);
-    // })
 
 
 
     // timer Function
     function timer() {
         if (!timerRunning)
-        clearInterval(intervalId);
+            clearInterval(intervalId);
         intervalId = setInterval(decrement, 1000);
         timerRunning = true;
     }
@@ -131,9 +165,14 @@ $(document).ready(function () {
 
             //  ...run the stop function. //move onto next question
             stop();
-
-            //  Alert the user that time is up.
-            alert("Time Up!");
+            counter++;
+            unansweredNum++;
+            var tooLate = $("<h3>Times Up! The correct answer was " + answerM.choice[answerM.answer] + "</h3>");
+            $("#answerArea").html(tooLate);
+            var imageReplace = $("<img>");
+            imageReplace.attr("src", answerM.photo);
+            imageReplace.addClass("col-12 col-lg-6");
+            $("#answerArea").append(imageReplace);
         }
     }
     //  The stop function
@@ -146,6 +185,6 @@ $(document).ready(function () {
         timerRunning = false;
     }
 
-   // timer();
+    // timer();
 
 })
